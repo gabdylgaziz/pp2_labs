003b495fa7b7201789f9ead0edd2f60accf866a3
Gabdylgaziz <98631613+gabdylgaziz@users.noreply.github.com>
2022-03-13 17:03:32 +0600

    Add files via upload

--- lab6/os/2.py
+++ lab6/os/2.py
@@ -2,6 +2,10 @@ import os
 path = input()
 try:
     os.chdir(path)
-    print(os.getcwd())
+    name = input()
+    print("Существует:", os.access(name, os.F_OK))
+    print("Доступен на чтение:", os.access(name, os.R_OK))
+    print("Доступен на запись:", os.access(name, os.W_OK))
+    print("Доступен на исполнение:", os.access(name, os.X_OK))
 except Exception as e:
     print(e)
